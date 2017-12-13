7132bfadbe83fe05e5e05dce7732cc8f954e45fd
Yogi Patel <yogesh.patel@mac.com>
2017-12-13 15:23:28 -0800

    js file

---
+++ javascript/crypto3.js
@@ -0,0 +1,25 @@
+
+
+<script src="https://www.gstatic.com/firebasejs/4.8.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAP5CCCilffpX4Ovga205pE4Ors6b1jDyk",
+    authDomain: "crypto3-424b9.firebaseapp.com",
+    databaseURL: "https://crypto3-424b9.firebaseio.com",
+    projectId: "crypto3-424b9",
+    storageBucket: "crypto3-424b9.appspot.com",
+    messagingSenderId: "1015211263452"
+  };
+  firebase.initializeApp(config);
+
+  
+</script>
+
+'buyCryptoThree' : function () {
+
+}
+
+'sellCryptoThree' : function () {
+
+}

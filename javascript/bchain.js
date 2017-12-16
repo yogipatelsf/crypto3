8290f07bd02fefeebf39d8d3861c07bd8146184f
A.Sofiane Guerfi <asofianeguerfi@ASofianes-MacBook-Pro.local>
2017-12-16 08:48:49 -0800

    Update

--- javascript/bchain.js
+++ javascript/bchain.js
@@ -9,15 +9,27 @@ class block{
 		this.data = data;
 		this.previousHash = previousHash;
 		this.hash = this.calculateHash();
+		this.nonce = 0;
 	}
 	calculateHash(){
-		return SHA256(this.index + this.previousHash + this.timestamp+JSON.stringify(this.data)).toString()
+		return SHA256(this.index + this.previousHash + this.timestamp+JSON.stringify(this.data) + this.nonce).toString()
+	}
+
+	mineBlock(difficulty){
+		while(this.hash.substring(0, difficulty)!== Array(difficulty + 1).join('0')){
+			this.nonce++;
+			this.hash = this.calculateHash();
+		}
+		console.log('Block mined: ' + this.hash);
 	}
 }
+
+
 
 class Blockchain{
 	constructor(){
 		this.chain = [this.createGenesisBlock()];
+		this.difficulty = 4;
 	}
 
 	createGenesisBlock(){
@@ -34,19 +46,41 @@ class Blockchain{
 	addBlock(newBlock){
 
 		newBlock.previousHash = this.getLatestBlock().hash;
-		newBlock.hash = newBlock.calculateHash();
+		newBlock.mineBlock(this.difficulty);
 		this.chain.push(newBlock);
+	}
+
+	isChainValid(){
+		for (var i = 1; i < this.chain.length; i++) {
+			const currentBlock = this.chain[i];
+			const previousBlock = this.chain[i - 1];
+//Checking if any of the data has been changed
+//First, data within the block
+			if (currentBlock.hash !== currentBlock.calculateHash()) {
+				return false;
+			}
+//Second, replacing the entire block----->>>breaking the chain!!
+			if (currentBlock.previousHash !== previousBlock.hash) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
 
 
 let crypto3 = new Blockchain();
-// console.log(crypto3.createGenesisBlock())
-
-
+//Display the Blockchain
+//mining the first block after the proof of work
+console.log('Mining block 1..');
 crypto3.addBlock(new block(1, new Date() , { amount: 5}));
+
+console.log('Mining block 2..');
 crypto3.addBlock(new block(2,new Date() , { amount: 2}));
-crypto3.addBlock(new block(3, new Date() , { amount: 2}));
-crypto3.addBlock(new block(4, new Date() , { amount: 2}));
-// console.log(crypto3.getLatestBlock());
-console.log(crypto3);
+//console.log(crypto3);
+
+//Check if the chain is valid------ try to hack it!;)
+// crypto3.chain[1].data = { amount: 100}; 
+// crypto3.chain[1].hash = crypto3.chain[1].calculateHash();
+console.log('Is Blockchain Valid? '+ crypto3.isChainValid());
+
